5a7f799c1bfefa3907c7d61529b48728eeef6791
이연주 <yunjoo0510@gmail.com>
2025-08-28 17:04:35 +0900

    lyj충돌 보냅니다

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('test_git.url')),
+    path('base', include('test_git.url')),      #lyj 브랜치로 바꿈
 ]
